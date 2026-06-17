a4a87ef02dc610c7f2779ac5b4bfd62c377c3053
chrispsk <chrispsk@gmail.com>
2026-06-17 18:00:14 +0100

    Make user_id field nullable in Contact model

--- contacts/models.py
+++ contacts/models.py
@@ -9,6 +9,6 @@ class Contacts(models.Model):
     phone = models.CharField(max_length=100)
     message = models.TextField(blank=True)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
-    user_id = models.IntegerField(blank=True)
+    user_id = models.IntegerField(blank=True, null=True)
     def __str__(self):
         return self.name
